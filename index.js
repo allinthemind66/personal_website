b1918c08c96ee834fb42fe29e6bb398595e68dde
allinthemind66 <r.nilon92@gmail.com>
2018-08-15 14:03:23 -0400

    add window pushState and popState functions

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ routes = {
   //'/contact': contactPage,
 };
 
-console.log(window.location.pathname)
+console.log(window.location.pathname);
 
 const contentDiv = document.getElementById('content');
-contentDiv.innerHTML = routes[window.location.pathname]
+contentDiv.innerHTML = routes[window.location.pathname];
+
+let onNavItemClick = (pathName) => {
+  window.history.pushState(
+    {},
+    pathName,
+    window.location.origin + pathName
+  );
+  contentDiv.innerHTML = routes[pathName];
+};
+
+window.onpopstate = () => {
+  contentDiv.innerHTML = routes[window.location.pathname];
+};
