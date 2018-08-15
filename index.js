51057407ab774847bc01691aaa6b5ec6a9a6d9a5
allinthemind66 <r.nilon92@gmail.com>
2018-08-15 13:58:37 -0400

    render home page to screen

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 routes = {
   '/': homePage,
-  '/portfolio': portfolioPage,
-  '/resume': resumePage,
-  '/contact': contactPage,
+  //'/portfolio': portfolioPage,
+  //'/resume': resumePage,
+  //'/contact': contactPage,
 };
+
+console.log(window.location.pathname)
+
+const contentDiv = document.getElementById('content');
+contentDiv.innerHTML = routes[window.location.pathname]
